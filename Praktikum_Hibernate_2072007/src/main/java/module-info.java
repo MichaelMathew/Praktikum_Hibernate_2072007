b2a9b203fff9ac35afa920006aa953d937160006
MichaelMathew <71740950+MichaelMathew@users.noreply.github.com>
2022-08-10 20:43:23 +0700

    Praktikum_Hibernate_2072007
    Praktikum_Hibernate_2072007
    Michael Mathew Setiadi

---
+++ Praktikum_Hibernate_2072007/src/main/java/module-info.java
@@ -0,0 +1,17 @@
+module com.example.praktikum_database_2072007 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.sql;
+    requires jasperreports;
+    requires java.naming;
+    requires org.hibernate.orm.core;
+    requires java.persistence;
+
+    opens com.example.praktikum_hibernate_2072007 to javafx.fxml;
+    exports com.example.praktikum_hibernate_2072007;
+    exports com.example.praktikum_hibernate_2072007.model;
+    exports com.example.praktikum_hibernate_2072007.dao;
+    exports com.example.praktikum_hibernate_2072007.Controller;
+    exports com.example.praktikum_hibernate_2072007.util;
+    opens com.example.praktikum_hibernate_2072007.model;
+}
